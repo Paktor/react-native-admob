abc33c0008f474fe4d2307c296a840c128596c7c
gary.lee <gary.lee@wetech.hk>
2023-06-15 17:24:26 +0800

    update implementation

--- android/src/main/java/com/sbugert/rnadmob/RNAdMobInterstitialAdModule.java
+++ android/src/main/java/com/sbugert/rnadmob/RNAdMobInterstitialAdModule.java
@@ -2,7 +2,7 @@ package com.sbugert.rnadmob;
 
 import android.os.Handler;
 import android.os.Looper;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
@@ -11,12 +11,12 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.InterstitialAd;
+//import com.google.android.gms.ads.AdListener;
+//import com.google.android.gms.ads.AdRequest;
+//import com.google.android.gms.ads.InterstitialAd;
 
 public class RNAdMobInterstitialAdModule extends ReactContextBaseJavaModule {
-  InterstitialAd mInterstitialAd;
+//  InterstitialAd mInterstitialAd;
   String adUnitID;
   String testDeviceID;
   Callback requestAdCallback;
@@ -29,55 +29,55 @@ public class RNAdMobInterstitialAdModule extends ReactContextBaseJavaModule {
 
   public RNAdMobInterstitialAdModule(ReactApplicationContext reactContext) {
     super(reactContext);
-    mInterstitialAd = new InterstitialAd(reactContext);
+//    mInterstitialAd = new InterstitialAd(reactContext);
 
-    new Handler(Looper.getMainLooper()).post(new Runnable() {
-      @Override
-      public void run() {
-        mInterstitialAd.setAdListener(new AdListener() {
-          @Override
-          public void onAdClosed() {
-            sendEvent("interstitialDidClose", null);
-            showAdCallback.invoke();
-          }
-          @Override
-          public void onAdFailedToLoad(int errorCode) {
-            WritableMap event = Arguments.createMap();
-            String errorString = null;
-            switch (errorCode) {
-              case AdRequest.ERROR_CODE_INTERNAL_ERROR:
-                errorString = "ERROR_CODE_INTERNAL_ERROR";
-                break;
-              case AdRequest.ERROR_CODE_INVALID_REQUEST:
-                errorString = "ERROR_CODE_INVALID_REQUEST";
-                break;
-              case AdRequest.ERROR_CODE_NETWORK_ERROR:
-                errorString = "ERROR_CODE_NETWORK_ERROR";
-                break;
-              case AdRequest.ERROR_CODE_NO_FILL:
-                errorString = "ERROR_CODE_NO_FILL";
-                break;
-            }
-            event.putString("error", errorString);
-            sendEvent("interstitialDidFailToLoad", event);
-            requestAdCallback.invoke(errorString);
-          }
-          @Override
-          public void onAdLeftApplication() {
-            sendEvent("interstitialWillLeaveApplication", null);
-          }
-          @Override
-          public void onAdLoaded() {
-            sendEvent("interstitialDidLoad", null);
-            requestAdCallback.invoke();
-          }
-          @Override
-          public void onAdOpened() {
-            sendEvent("interstitialDidOpen", null);
-          }
-        });
-      }
-    });
+//    new Handler(Looper.getMainLooper()).post(new Runnable() {
+//      @Override
+//      public void run() {
+//        mInterstitialAd.setAdListener(new AdListener() {
+//          @Override
+//          public void onAdClosed() {
+//            sendEvent("interstitialDidClose", null);
+//            showAdCallback.invoke();
+//          }
+//          @Override
+//          public void onAdFailedToLoad(int errorCode) {
+//            WritableMap event = Arguments.createMap();
+//            String errorString = null;
+//            switch (errorCode) {
+//              case AdRequest.ERROR_CODE_INTERNAL_ERROR:
+//                errorString = "ERROR_CODE_INTERNAL_ERROR";
+//                break;
+//              case AdRequest.ERROR_CODE_INVALID_REQUEST:
+//                errorString = "ERROR_CODE_INVALID_REQUEST";
+//                break;
+//              case AdRequest.ERROR_CODE_NETWORK_ERROR:
+//                errorString = "ERROR_CODE_NETWORK_ERROR";
+//                break;
+//              case AdRequest.ERROR_CODE_NO_FILL:
+//                errorString = "ERROR_CODE_NO_FILL";
+//                break;
+//            }
+//            event.putString("error", errorString);
+//            sendEvent("interstitialDidFailToLoad", event);
+//            requestAdCallback.invoke(errorString);
+//          }
+//          @Override
+//          public void onAdLeftApplication() {
+//            sendEvent("interstitialWillLeaveApplication", null);
+//          }
+//          @Override
+//          public void onAdLoaded() {
+//            sendEvent("interstitialDidLoad", null);
+//            requestAdCallback.invoke();
+//          }
+//          @Override
+//          public void onAdOpened() {
+//            sendEvent("interstitialDidOpen", null);
+//          }
+//        });
+//      }
+//    });
   }
   private void sendEvent(String eventName, @Nullable WritableMap params) {
     getReactApplicationContext().getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName, params);
@@ -85,7 +85,7 @@ public class RNAdMobInterstitialAdModule extends ReactContextBaseJavaModule {
 
   @ReactMethod
   public void setAdUnitID(String adUnitID) {
-    mInterstitialAd.setAdUnitId(adUnitID);
+//      mInterstitialAd.setAdUnitId(adUnitID);
   }
 
   @ReactMethod
@@ -95,26 +95,26 @@ public class RNAdMobInterstitialAdModule extends ReactContextBaseJavaModule {
 
   @ReactMethod
   public void requestAd(final Callback callback) {
-    new Handler(Looper.getMainLooper()).post(new Runnable() {
-      @Override
-      public void run () {
-        if (mInterstitialAd.isLoaded() || mInterstitialAd.isLoading()) {
-          callback.invoke("Ad is already loaded."); // TODO: make proper error
-        } else {
-          requestAdCallback = callback;
-          AdRequest.Builder adRequestBuilder = new AdRequest.Builder();
-          if (testDeviceID != null){
-            if (testDeviceID.equals("EMULATOR")) {
-              adRequestBuilder = adRequestBuilder.addTestDevice(AdRequest.DEVICE_ID_EMULATOR);
-            } else {
-              adRequestBuilder = adRequestBuilder.addTestDevice(testDeviceID);
-            }
-          }
-          AdRequest adRequest = adRequestBuilder.build();
-          mInterstitialAd.loadAd(adRequest);
-        }
-      }
-    });
+//    new Handler(Looper.getMainLooper()).post(new Runnable() {
+//      @Override
+//      public void run () {
+//        if (mInterstitialAd.isLoaded() || mInterstitialAd.isLoading()) {
+//          callback.invoke("Ad is already loaded."); // TODO: make proper error
+//        } else {
+//          requestAdCallback = callback;
+//          AdRequest.Builder adRequestBuilder = new AdRequest.Builder();
+//          if (testDeviceID != null){
+//            if (testDeviceID.equals("EMULATOR")) {
+//              adRequestBuilder = adRequestBuilder.addTestDevice(AdRequest.DEVICE_ID_EMULATOR);
+//            } else {
+//              adRequestBuilder = adRequestBuilder.addTestDevice(testDeviceID);
+//            }
+//          }
+//          AdRequest adRequest = adRequestBuilder.build();
+//          mInterstitialAd.loadAd(adRequest);
+//        }
+//      }
+//    });
   }
 
   @ReactMethod
@@ -122,12 +122,12 @@ public class RNAdMobInterstitialAdModule extends ReactContextBaseJavaModule {
     new Handler(Looper.getMainLooper()).post(new Runnable() {
       @Override
       public void run () {
-        if (mInterstitialAd.isLoaded()) {
-          showAdCallback = callback;
-          mInterstitialAd.show();
-        } else {
-          callback.invoke("Ad is not ready."); // TODO: make proper error
-        }
+//        if (mInterstitialAd.isLoaded()) {
+//          showAdCallback = callback;
+//          mInterstitialAd.show();
+//        } else {
+//          callback.invoke("Ad is not ready."); // TODO: make proper error
+//        }
       }
     });
   }
@@ -137,7 +137,7 @@ public class RNAdMobInterstitialAdModule extends ReactContextBaseJavaModule {
     new Handler(Looper.getMainLooper()).post(new Runnable() {
       @Override
       public void run () {
-        callback.invoke(mInterstitialAd.isLoaded());
+//        callback.invoke(mInterstitialAd.isLoaded());
       }
     });
   }
